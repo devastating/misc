303a6997a397aec9f24c610ccce72355c0efa33b
nick <nick@localhost.localdomain>
2016-03-14 11:08:47 -0700

    Shell script example
    - List all Python byte files without corresponding Python source file

---
+++ shell_scripts/list_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List Python files in current directory that only has .pyc, but no .py
+
+# 1. in ls, use --time-style to fix the output format
+# 2. egrep - extension grep for regular expression
+# 3. last egrep makes sure files left are with extension
+# 4. sort the files
+PY_FILES=`ls -lR --time-style="long-iso" | egrep '^-' | awk '{print $8}' | egrep '(*\.py|*\.pyc|*\.pyo)' | sort`
+PY_SOUCE=""
+for PY_FILE in ${PY_FILES}
+do
+    if [[ "$PY_FILE" =~ py$ ]]; then
+        PY_SOURCE="${PY_FILE%.*}"
+    else
+        py_file="${PY_FILE%.*}"
+        if [ "$py_file" != "$PY_SOURCE" ]; then
+            echo "Found no-source Python file $PY_FILE"
+        fi
+    fi
+done
